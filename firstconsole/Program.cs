b5c3b7697e1671059291d8d4c7cc2c62af9e9db4
Elena <elena.st463@gmail.com>
2020-01-04 20:58:23 +0100

    Creating a console application

---
+++ firstconsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace firstconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello, Lena!";
+            Console.WriteLine(message);
+            Console.WriteLine("Press eny key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
