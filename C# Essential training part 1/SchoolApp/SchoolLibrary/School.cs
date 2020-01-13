083c71518d279eb7dab242dd0c466895d821fa03
stankevo1 <elena.st463@gmail.com>
2020-01-13 16:46:49 +0100

    3.1 Creating a class

---
+++ C# Essential training part 1/SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string SchoolName;
+        string SchoolAddress;
+        string SchoolCity;
+        string SchoolState;
+        string SchoolZip;
+        string PhoneName;
+    }
+}
