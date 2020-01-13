f50f4a3500ffce8d20f3bb3b940c4a18119c1049
Elena <elena.st463@gmail.com>
2020-01-13 21:37:29 +0100

    C# Essential training part1

---
+++ C# Essential training part 1/2 - Getting started/UnderstandingSyntax/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UnderstandingSyntax
+{
+    class Program
+    {
+        public static string title = "C# Essential Training";
+        public static int whichPart;
+
+        // C# types
+        int firstInt = 5;
+        
+        
+        static void Main(string[] args)
+        {
+            whichPart = 2;
+            var message = string.Format("Welcome to {0} Part {1}!", title, whichPart);
+            Console.WriteLine(message);
+            Console.WriteLine("Press the enter key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
