c505f0959d4bc5fcb06e9350a630b8479831c11e
stankevo1 <elena.st463@gmail.com>
2020-01-14 15:00:12 +0100

    3.4 Creating properties2

--- C# Essential training part 1/SchoolApp/SchoolLibrary/School.cs
+++ C# Essential training part 1/SchoolApp/SchoolLibrary/School.cs
@@ -12,7 +12,21 @@ namespace SchoolLibrary
         string State { get; set; }
         string Zip { get; set; }
         string PhoneName { get; set; }
-        
 
+        string _twitterAddress;
+        string TwitterAddress
+        {
+            get { return _twitterAddress; }
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                } else
+                {
+                    throw new Exception("The twitter address should start with @.")
+                }
+            }
+        }
     }
 }
