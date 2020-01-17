4bab85df65c6e9be037850a1c6996c4b84d6d96a
stankevo_h <elena.st463@gmail.com>
2020-01-17 23:02:31 +0200

    3.12 Accessing the school class

--- C# Essential training part 1/SchoolApp/SchoolFormsApp/Form1.Designer.cs
+++ C# Essential training part 1/SchoolApp/SchoolFormsApp/Form1.Designer.cs
@@ -28,20 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblSchoolName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblZip = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblTwitterAddress = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtTwitterAddress = new System.Windows.Forms.TextBox();
+            this.btnPushToTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblSchoolName
+            // 
+            this.lblSchoolName.AutoSize = true;
+            this.lblSchoolName.Location = new System.Drawing.Point(25, 13);
+            this.lblSchoolName.Name = "lblSchoolName";
+            this.lblSchoolName.Size = new System.Drawing.Size(71, 13);
+            this.lblSchoolName.TabIndex = 0;
+            this.lblSchoolName.Text = "School Name";
+            this.lblSchoolName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(51, 47);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 1;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(72, 82);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 2;
+            this.lblCity.Text = "City";
+            // 
+            // lblZip
+            // 
+            this.lblZip.AutoSize = true;
+            this.lblZip.Location = new System.Drawing.Point(72, 155);
+            this.lblZip.Name = "lblZip";
+            this.lblZip.Size = new System.Drawing.Size(22, 13);
+            this.lblZip.TabIndex = 3;
+            this.lblZip.Text = "Zip";
+            this.lblZip.Click += new System.EventHandler(this.lblZip_Click);
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(23, 193);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(78, 13);
+            this.lblPhone.TabIndex = 4;
+            this.lblPhone.Text = "Phone Number";
+            this.lblPhone.Click += new System.EventHandler(this.lblPhone_Click);
+            // 
+            // lblTwitterAddress
+            // 
+            this.lblTwitterAddress.AutoSize = true;
+            this.lblTwitterAddress.Location = new System.Drawing.Point(21, 228);
+            this.lblTwitterAddress.Name = "lblTwitterAddress";
+            this.lblTwitterAddress.Size = new System.Drawing.Size(80, 13);
+            this.lblTwitterAddress.TabIndex = 5;
+            this.lblTwitterAddress.Text = "Twitter Address";
+            this.lblTwitterAddress.Click += new System.EventHandler(this.lblTwitterAddress_Click);
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(64, 118);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(32, 13);
+            this.lblState.TabIndex = 6;
+            this.lblState.Text = "State";
+            this.lblState.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(107, 10);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(129, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(107, 44);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(129, 20);
+            this.txtAddress.TabIndex = 8;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(107, 79);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(129, 20);
+            this.txtCity.TabIndex = 9;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(107, 115);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(129, 20);
+            this.txtState.TabIndex = 10;
+            this.txtState.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(107, 152);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(129, 20);
+            this.txtZip.TabIndex = 11;
+            this.txtZip.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(107, 190);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(129, 20);
+            this.txtPhone.TabIndex = 12;
+            this.txtPhone.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // txtTwitterAddress
+            // 
+            this.txtTwitterAddress.Location = new System.Drawing.Point(107, 225);
+            this.txtTwitterAddress.Name = "txtTwitterAddress";
+            this.txtTwitterAddress.Size = new System.Drawing.Size(129, 20);
+            this.txtTwitterAddress.TabIndex = 13;
+            this.txtTwitterAddress.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // btnPushToTest
+            // 
+            this.btnPushToTest.Location = new System.Drawing.Point(107, 262);
+            this.btnPushToTest.Name = "btnPushToTest";
+            this.btnPushToTest.Size = new System.Drawing.Size(80, 23);
+            this.btnPushToTest.TabIndex = 14;
+            this.btnPushToTest.Text = "Push To Test";
+            this.btnPushToTest.UseVisualStyleBackColor = true;
+            this.btnPushToTest.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(281, 232);
+            this.ClientSize = new System.Drawing.Size(281, 308);
+            this.Controls.Add(this.btnPushToTest);
+            this.Controls.Add(this.txtTwitterAddress);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtZip);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblTwitterAddress);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblZip);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblSchoolName);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSchoolName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblZip;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblTwitterAddress;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtTwitterAddress;
+        private System.Windows.Forms.Button btnPushToTest;
     }
 }
 
--- C# Essential training part 1/SchoolApp/SchoolFormsApp/Form1.cs
+++ C# Essential training part 1/SchoolApp/SchoolFormsApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SchoolLibrary;
 
 namespace SchoolFormsApp
 {
@@ -15,6 +16,59 @@ namespace SchoolFormsApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTwitterAddress_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPhone_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblZip_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var testSchool = new School();
+            testSchool.Name = "Central City High";
+            //testSchool.TwitterAddress = "@CentralCityHigh";
+            testSchool.TwitterAddress = "wrong twitter";
         }
     }
 }
