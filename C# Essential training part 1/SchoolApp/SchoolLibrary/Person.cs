9fe8fec9e12a7bc9c7019708507260e4a8adb0bc
stankevo_h <elena.st463@gmail.com>
2020-01-18 14:07:40 +0200

    4.5 Virtual methods

--- C# Essential training part 1/SchoolApp/SchoolLibrary/Person.cs
+++ C# Essential training part 1/SchoolApp/SchoolLibrary/Person.cs
@@ -11,5 +11,15 @@ namespace SchoolLibrary
         public string Email { get; set; }
 
         public abstract float ComputeGradeAverage();
+
+        public virtual string SendMessage(string message)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Format("The message was sent on {0:D}, at {1:t}", DateTime.Now));
+            sb.AppendLine("");
+            sb.AppendLine("Dear " + FirstName + ",");
+            sb.AppendLine(message);
+            return sb.ToString();
+        }
     }
 }
