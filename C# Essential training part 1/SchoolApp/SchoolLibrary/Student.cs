5711c46932186ccb7337430b5c159ab845319e53
stankevo_h <elena.st463@gmail.com>
2020-01-18 13:16:41 +0200

    4.1 Extending a class to create a new class

---
+++ C# Essential training part 1/SchoolApp/SchoolLibrary/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Student : Person
+    {
+        public enum GradeLevel { FreshMan, Sophomore, Junior, Senior }
+        public GradeLevel grade { get; set; }
+    }
+}
