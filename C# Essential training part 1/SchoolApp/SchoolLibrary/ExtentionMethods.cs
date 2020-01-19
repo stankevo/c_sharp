a9bb309657e29f17d9d88e3204f830224c922ed5
stankevo_h <elena.st463@gmail.com>
2020-01-19 15:36:21 +0200

    4.10 Extesion methods

---
+++ C# Essential training part 1/SchoolApp/SchoolLibrary/ExtentionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public static class ExtentionMethods
+    {
+        public static int WordCount(this string str)
+        {
+            var WordCount = str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            return WordCount;
+        }
+    }
+}
