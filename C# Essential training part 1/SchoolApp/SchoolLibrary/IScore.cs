962d56cce7ae64c9227b140849fe3aa4728eeb2d
stankevo_h <elena.st463@gmail.com>
2020-01-19 14:39:50 +0200

    4.8 Adding an interface

---
+++ C# Essential training part 1/SchoolApp/SchoolLibrary/IScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public interface IScore
+    {
+        float Score { get; set; }
+        float MaximumScore { get; set; }
+    }
+}
--- C# Essential training part 1/SchoolApp/SchoolLibrary/ScienceExperiment.cs
+++ C# Essential training part 1/SchoolApp/SchoolLibrary/ScienceExperiment.cs
@@ -4,11 +4,13 @@ using System.Text;
 
 namespace SchoolLibrary
 {
-    public class ScienceExperiment
+    public class ScienceExperiment : IScore
     {
         public string Hypothesis { get; set; }
         public string Materials { get; set; }
         public string Method { get; set; }
         public string Conclusion { get; set; }
+        public float Score { get; set; }
+        public float MaximumScore { get; set; }
     }
 }
