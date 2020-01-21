16d7d6ac144f650a4546e4b9414bb2864a0e681f
stankevo_h <elena.st463@gmail.com>
2020-01-22 00:28:59 +0200

    2. Working with arrays

---
+++ C# Essentialtraining part 2/EssentialTraining2/UnitTestProject1/SimpleArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class SimpleArray
+    {
+        public string[] GroceryList;
+
+        public SimpleArray()
+        {
+            GroceryList = new string[4] { "Milk", "Bread", "Eggs", "Cheese" };
+
+        public override string ToString()
+        {
+            return "The grocery list contains " + GroceryList.Length + " items: " + GroceryList.ToString();
+        }
+    }
+    }
+}
