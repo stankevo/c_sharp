6a5c456ea59eed96cacb1718a4d14b7f138c1c7a
stankevo_h <elena.st463@gmail.com>
2020-02-09 19:18:26 +0200

    Learning C# Algorithms: 1. Linked List

---
+++ Learning C# Algorithms/MyLinkedListConsoleApp/MyLinkedListConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedListConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLikedList a = new SinglyLikedList();
+            a.InsertFirst(1005);
+            a.InsertFirst(1004);
+            a.InsertFirst(1003);
+            a.InsertFirst(1002);
+            a.InsertFirst(1001);
+
+            Console.WriteLine("List a:");
+            a.DisplayList();
+
+            Console.WriteLine("");
+            Console.WriteLine("Delete first:");
+            a.DeleteFirst();
+            a.DisplayList();
+
+            Console.WriteLine("");
+            Console.WriteLine("Insert last:");
+            a.InsertLast(1006);
+            a.DisplayList();
+
+            Console.WriteLine("");
+            Console.WriteLine("Delete 5 elements one by one:");
+            a.DeleteFirst();
+            a.DeleteFirst();
+            a.DeleteFirst();
+            a.DeleteFirst();
+            a.DeleteFirst();
+            a.DeleteFirst();
+
+            a.DisplayList();
+
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Use c# built in lists:");
+            var b = new LinkedList<int>();
+            b.AddFirst(2001);
+            b.AddLast(2002);
+            b.AddAfter(b.Find(2001), 20015);
+
+            var item = b.First;
+            while (item != null)
+            {
+                Console.WriteLine("< " + item.Value + ", " + ((item.Next != null) ? item.Next.Value.ToString() : "_") + " >");
+                item = item.Next;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning C# Algorithms/MyLinkedListConsoleApp/MyLinkedListConsoleApp/SinglyLikedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedListConsoleApp
+{
+    public class SinglyLikedList
+    {
+        public Node Head { get; set; }
+
+        public void InsertFirst(int value)
+        {
+            Node newNode = new Node
+            {
+                Value = value,
+                Next = Head
+            };
+
+            Head = newNode;
+        }
+
+        public void DeleteFirst()
+        {
+            try
+            {
+                Head = Head.Next;
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("There are no more elements in list");
+            }
+        }
+        
+        public void DisplayList()
+        {
+            var item = Head;
+            while (item != null) 
+            {
+                Console.WriteLine("< " + item.Value + ", " + ((item.Next != null)? item.Next.Value.ToString() : "_") + " >");
+                item = item.Next;
+            }
+        }
+
+        public void InsertLast(int value)
+        {
+            var newItem = new Node
+            {
+                Value = value
+            };
+
+            var item = Head;
+            while (item.Next != null)
+            {
+                item = item.Next;
+            }
+            item.Next = newItem;
+        }
+    }
+
+
+}
