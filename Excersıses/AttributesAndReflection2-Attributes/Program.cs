bca3e5ce70171530140dcd8afcd047e7d873d171
stankevo1 <elena.st463@gmail.com>
2020-03-15 20:23:46 +0100

    Attributes and reflections

---
+++ Excersıses/AttributesAndReflection2-Attributes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributesAndReflection2_Attributes
+{
+    class MeAttribute : Attribute // this is a type
+    {
+        public MeAttribute(int value, string secondValue) // constructor
+        {
+            Console.WriteLine("MeAttribute()");
+            Console.WriteLine(value);
+            Console.WriteLine(secondValue);
+        }
+
+        public int SomeIntProperty { get; set; }
+        public char SomeCharProperty { get; set; }
+    }
+
+    [Me(25, "Jamie King", SomeCharProperty = 'J', SomeIntProperty = 72)] // we can provide parameters to the constructor and instantiate other properties
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            typeof(Program).GetCustomAttributes(false);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Excersıses/AttributesAndReflection_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributesAndReflection_1
+{
+    class TestAttribute : Attribute { }
+    class TestMethodAttribute : Attribute { }
+
+    [Test]
+    class MyTestSuite
+    {
+        public void HelperMethod()
+        {
+            // That helps your tests gt their job done...
+            Console.WriteLine("This method will never be invoked because it does not have a TestMethodAttribute on it.");
+        }
+
+        [TestMethod]
+        public void MyTest1()
+        {
+            Console.WriteLine("Doing some testing... ");
+        }
+
+        [TestMethod]
+        public void MyTest2()
+        {
+            Console.WriteLine("Doing some other testing...");
+        }
+
+    }
+
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            // find all test suites in this assembly (classes which are marked by TestAttribute)
+            var testSuites =
+                from t in Assembly.GetExecutingAssembly().GetTypes()
+                where t.GetCustomAttributes(false).Any(a => a is TestAttribute)
+                select t;
+
+            // for each test suite:
+            foreach (Type t in testSuites)
+            {
+                Console.WriteLine("Running tests in suite " + t.Name);
+                // find all those methods which are test methods (methds which are marked by TestMethodAttribute)
+                var testMethods =
+                    from m in t.GetMethods()
+                    where m.GetCustomAttributes(false).Any(a => a is TestMethodAttribute)
+                    select m;
+
+                // instantiate the test suite class
+                object testSuiteInstance = Activator.CreateInstance(t);
+
+                // start all test methods from this test suite
+                foreach (MethodInfo mInfo in testMethods)
+                    mInfo.Invoke(testSuiteInstance, new object[0]);
+            }
+                
+
+            Console.ReadLine();
+        }
+    }
+}
