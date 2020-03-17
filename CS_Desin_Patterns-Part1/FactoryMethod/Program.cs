1aa354634a5f2be1a552704ede349ae5828e6411
stankevo1 <elena.st463@gmail.com>
2020-03-17 22:15:32 +0100

    C# Design Patterns: Part 1 - Factory Method

---
+++ CS_Desin_Patterns-Part1/FactoryMethod/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class Program
+    {
+       
+
+    }
+
+    public abstract class ISavingsAccount
+    {
+        public decimal Balance { get; set; }
+    }
+
+    public class CitiSavingsAccount : ISavingsAccount
+    {
+        public CitiSavingsAccount()
+        {
+            Balance = 5000;
+        }
+    }
+
+    public class NationalSavingsAccount : ISavingsAccount
+    {
+        public NationalSavingsAccount()
+        {
+            Balance = 2000;
+        }
+    }
+
+    interface ICreditUnionFactory
+    {
+        ISavingsAccount GetSavingsAccount(string acctNo);
+    }
+
+    public class SavingdsAccountFactory : ICreditUnionFactory
+    {
+        public ISavingsAccount GetSavingsAccount(string acctNo)
+        {
+            if (acctNo.Contains("CITI"))
+                return new CitiSavingsAccount();
+            else if (acctNo.Contains("NATIONAL"))
+                return new NationalSavingsAccount();
+            else
+                throw new ArgumentException("Invalid account number.");
+        }
+    }
+    
+}
---
+++ CS_Desin_Patterns-Part1/Factory_Method/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new SavingsAccountFactory() as ICreditUnionFactory; //compile-time type is ICreditUnionFactory, but run-time type is SavingsAccountFactory
+            var myCitiAcount = factory.GetSavingsAccount("CITI-1234"); //compile-time type is ISavingsAccount, but run-time type is CitiSavingsAccount
+            var myNationalAccount = factory.GetSavingsAccount("NATIONAL-1234"); //compile-time type is ISavingsAccount, but run-time type is NationalSavingsAccount
+
+            Console.WriteLine("My Citi account balance is " + myCitiAcount.Balance +
+                            "\nMy National account balance is " + myNationalAccount.Balance);
+
+            //Console.WriteLine("myCitiAcount run time type is " + myCitiAcount.GetType() +
+            //                "\nmyNationalAccount run time type is " + myNationalAccount.GetType() +
+            //                "\nfactory run time type is " + factory.GetType());
+
+            Console.ReadLine();
+        }
+    }
+
+
+    //Product
+    public abstract class ISavingsAccount
+    {
+        public decimal Balance { get; set; }
+    }
+
+    //Concrete product
+    public class CitiSavingsAccount : ISavingsAccount
+    {
+        public CitiSavingsAccount()
+        {
+            Balance = 5000;
+        }
+    }
+
+    //Another concrete prodict
+    public class NationalSavingsAccount : ISavingsAccount
+    {
+        public NationalSavingsAccount()
+        {
+            Balance = 2000;
+        }
+    }
+
+    //Creator or Factory
+    interface ICreditUnionFactory
+    {
+        ISavingsAccount GetSavingsAccount(string acctNo);
+    }
+
+    //Concrete creator
+    public class SavingsAccountFactory : ICreditUnionFactory
+    {
+        public ISavingsAccount GetSavingsAccount(string acctNo)
+        {
+            if (acctNo.Contains("CITI"))
+                return new CitiSavingsAccount();
+            else if (acctNo.Contains("NATIONAL"))
+                return new NationalSavingsAccount();
+            else
+                throw new ArgumentException("Unexpected account format.");
+        }
+    }
+
+}
